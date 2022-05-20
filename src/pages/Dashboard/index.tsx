e098f892de296f8910ea8bbbbf7bd23ae19054df
John <jhow.mito11@gmail.com>
2022-05-20 13:11:13 -0300

    Atualization

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
-import {Title} from './styles'
+
+import logoImg from '../../assets/logo.svg'
+import Repository from '../Repository'
+
+import { Title, Form , Repositories} from './styles'
 
 const Dashboard: React.FC = () => {
-return <Title>Explore repositórios no Github</Title>
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no Github</Title>
+
+            <Form> 
+
+            <input placeholder='Digite o nome do repositório'/>
+            <button type = "submit"> Pesquisar </button>
+            </Form>
+
+<Repositories>
+<a href='test' > 
+<img
+src="https://avatars.githubusercontent.com/u/84046012?v=4"
+alt='John Santos'
+/>
+</a>
+</Repositories>
+
+        </>
+    )
 }
 
 export default Dashboard
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,6 +1,45 @@
+import { shade } from "polished";
 import styled from "styled-components";
 
 export const Title = styled.h1 `
 font-size: 48px;
-color: #3a3a3a
+color: #3a3a3a;
+max-width: 450px;
+line-height: 56px;
+
+margin-top: 80px
 `;
+
+
+export const Form = styled.form `
+margin-top: 40px;
+max-width: 700px;
+
+display: flex;
+input {
+    flex:1;
+    padding: 0 24px;
+    border: 0;
+    border-radius: 5px 0 0 5px
+}
+
+button {
+    width: 210px;
+    height: 70px;
+    border-radius: 0px 5px 5x 0px;
+    background: #04d361;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+
+    transition: background-color 0.2s;
+
+&:hover {
+  background: ${shade(0.2, '#04d361')};
+}
+}
+`;
+
+export const Repositories = styled.div`
+
+`
