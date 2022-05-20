7bfe57042622805efb3bafe6513ec72f4d8d9bb7
John Santos <jhow.mito11@gmail.com>
2022-05-20 19:29:38 -0300

    Requisition

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,30 +1,93 @@
-import React from 'react'
-
+import React, { useState, FormEvent } from 'react'
+import { FiChevronRight } from 'react-icons/fi'
+import api from '../../services/api'
 import logoImg from '../../assets/logo.svg'
-import Repository from '../Repository'
 
-import { Title, Form , Repositories} from './styles'
+interface Repository {
+full_name: string;
+
+}
+
+import { Title, Form, Repositories } from './styles'
 
 const Dashboard: React.FC = () => {
+
+    const [newRepo, setNewRepo] = useState('')
+    //temos que tipar todo o estado que nao for padrão, temos que tipar de array e objetos 
+    const [repositories, setRepositories] = useState([])
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+        event.preventDefault()
+        console.log(newRepo)
+        // Adição de um novo repositorio
+        // consumir API do Github
+        // salvar novo repositorio no estado
+
+        const response = await api.get(`repos/${newRepo}`);
+        console.log(response.data)
+
+        const repository = response.data
+
+        setRepositories([...repositories, repository])
+
+    }
+
     return (
         <>
             <img src={logoImg} alt="Github Explorer" />
             <Title>Explore repositórios no Github</Title>
 
-            <Form> 
+            <Form onSubmit={handleAddRepository}>
 
-            <input placeholder='Digite o nome do repositório'/>
-            <button type = "submit"> Pesquisar </button>
+                <input
+                    value={newRepo}
+                    onChange={(e) => setNewRepo(e.target.value)}
+                    placeholder='Digite o nome do repositório'
+                />
+                <button type="submit"> Pesquisar </button>
             </Form>
 
-<Repositories>
-<a href='test' > 
-<img
-src="https://avatars.githubusercontent.com/u/84046012?v=4"
-alt='John Santos'
-/>
-</a>
-</Repositories>
+            <Repositories>
+                <a href='test' >
+                    <img
+                        src="https://avatars.githubusercontent.com/u/84046012?v=4"
+                        alt='John Santos'
+                    />
+                    <div>
+                        <strong> rocketseat/unform </strong>
+                        <p> Performance-focused API for React forms 🚀</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories >
+
+            <Repositories>
+                <a href='test' >
+                    <img
+                        src="https://avatars.githubusercontent.com/u/84046012?v=4"
+                        alt='John Santos'
+                    />
+                    <div>
+                        <strong> rocketseat/unform </strong>
+                        <p> Performance-focused API for React forms 🚀</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories >
+
+            <Repositories>
+                <a href='test' >
+                    <img
+                        src="https://avatars.githubusercontent.com/u/84046012?v=4"
+                        alt='John Santos'
+                    />
+                    <div>
+                        <strong> rocketseat/unform </strong>
+                        <p> Performance-focused API for React forms 🚀</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </a>
+            </Repositories >
 
         </>
     )
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,7 +1,7 @@
 import { shade } from "polished";
 import styled from "styled-components";
 
-export const Title = styled.h1 `
+export const Title = styled.h1`
 font-size: 48px;
 color: #3a3a3a;
 max-width: 450px;
@@ -11,7 +11,7 @@ margin-top: 80px
 `;
 
 
-export const Form = styled.form `
+export const Form = styled.form`
 margin-top: 40px;
 max-width: 700px;
 
@@ -41,5 +41,54 @@ button {
 `;
 
 export const Repositories = styled.div`
+  margin-top: 80px;
+  max-width: 700px;
 
-`
+  a {
+    background: #fff;
+    border-radius: 5px;
+    width: 100%;
+    padding: 24px;
+    display: block;
+    text-decoration: none;
+
+    display: flex;
+    align-items: center;
+    transition: transform 0.2s;
+
+    &:hover {
+      transform: translateX(10px);
+    }
+
+    & + a {
+      margin-top: 16px;
+    }
+
+    img {
+      width: 64px;
+      height: 64px;
+      border-radius: 50%;
+    }
+
+    div {
+      margin: 0 16px;
+      flex: 1;
+
+      strong {
+        font-size: 20px;
+        color: #3d3d4d;
+      }
+
+      p {
+        font-size: 18px;
+        color: #a8a8b3;
+        margin-top: 4px;
+      }
+    }
+
+    svg {
+      margin-left: auto;
+      color: #cbcbd6;
+    }
+  }
+`;
